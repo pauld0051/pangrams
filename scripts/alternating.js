376a07850108a2ae6a47e3cae35ccb111fad9d59
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-26 09:58:33 +0000

    Add copy button for alternating text

--- scripts/alternating.js
+++ scripts/alternating.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Existing pangram checker code...
-
     // Alternating text maker
     document.getElementById('alternatingTextForm').addEventListener('submit', function (e) {
         e.preventDefault();
@@ -8,12 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
         const inputText = document.getElementById('textInput').value;
         let alternatingText = '';
 
+        // Convert to alternating case
         for (let i = 0; i < inputText.length; i++) {
-            // Convert to alternating case
             alternatingText += i % 2 === 0 ? inputText[i].toLowerCase() : inputText[i].toUpperCase();
         }
 
-        const resultDiv = document.getElementById('alternatingTextResult');
-        resultDiv.textContent = alternatingText;
+        // Set the alternating text as the value of the output textbox
+        const outputTextbox = document.getElementById('alternatingTextOutput');
+        outputTextbox.value = alternatingText;
+    });
+
+    // Function to copy text to clipboard
+    function copyToClipboard(text) {
+        const dummy = document.createElement('textarea');
+        document.body.appendChild(dummy);
+        dummy.value = text;
+        dummy.select();
+        document.execCommand('copy');
+        document.body.removeChild(dummy);
+    }
+
+    // Event listener for the copy button
+    document.getElementById('copyButton').addEventListener('click', function () {
+        const textToCopy = document.getElementById('alternatingTextOutput').value;
+        copyToClipboard(textToCopy);
     });
 });
