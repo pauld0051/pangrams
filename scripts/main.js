edd1f573948042e04537429b9c56fc340ff782b8
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-27 19:46:21 +0000

    Adjusted CSS and JS for header

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const dropdown = document.querySelector('.menu-item-dropdown > a');
-    const dropdownContent = document.querySelector('.submenu');
+    const mainMenuToggle = document.querySelector('.header-content > .menu-toggle');
+    const dropdownToggle = document.querySelector('.menu-item-dropdown > a');
+    const mainMenu = document.querySelector('.main-menu');
+    const submenu = document.querySelector('.submenu');
 
     // Toggle main menu
-    menuToggle.addEventListener('click', function () {
-        this.classList.toggle('active');
-        const mainMenu = document.querySelector('.main-menu');
+    mainMenuToggle.addEventListener('click', function () {
         mainMenu.classList.toggle('show');
     });
 
     // Toggle submenu on mobile
-    dropdown.addEventListener('click', function (event) {
+    dropdownToggle.addEventListener('click', function (event) {
         // Prevent default link behaviour only on mobile
         if (window.innerWidth <= 768) {
             event.preventDefault();
-            dropdownContent.classList.toggle('show');
+            submenu.classList.toggle('show');
         }
     });
 
     // Close the submenu if clicking outside of it
     window.addEventListener('click', function (event) {
         if (!event.target.matches('.menu-item-dropdown > a') && !event.target.closest('.submenu')) {
-            dropdownContent.classList.remove('show');
+            submenu.classList.remove('show');
         }
     });
 
     // Close dropdown when resizing to wider screens
     window.addEventListener('resize', function () {
-        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        if (screenWidth > 768) {
-            dropdownContent.classList.remove('show');
+        if (window.innerWidth > 768) {
+            submenu.classList.remove('show');
         }
     });
 });
