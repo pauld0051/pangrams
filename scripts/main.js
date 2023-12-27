30a99417425fb3b9c03632a5b32e97abedcbb1f3
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-27 21:18:20 +0000

    Adjust to new flavour header

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,22 @@
+// main.js
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const mainMenu = document.querySelector('.main-menu');
 
-    // Toggle main menu
     menuToggle.addEventListener('click', function () {
         mainMenu.classList.toggle('show');
     });
 
-    // Add event listeners to all dropdown toggles
-    document.querySelectorAll('.menu-item-dropdown').forEach(function (dropdown) {
-        const toggle = dropdown.querySelector('a');
-        const submenu = dropdown.querySelector('.submenu');
+    // Handle submenus for mobile
+    const dropdownToggles = document.querySelectorAll('.menu-item-dropdown > a');
 
+    dropdownToggles.forEach(toggle => {
         toggle.addEventListener('click', function (event) {
-            if (window.innerWidth <= 768) {
-                event.preventDefault(); // Prevent the default anchor behavior
-                submenu.classList.toggle('show'); // Toggle the `.show` class on the submenu
+            const submenu = this.nextElementSibling;
+            if (submenu && submenu.classList.contains('submenu')) {
+                event.preventDefault(); // Prevent navigation
+                submenu.classList.toggle('active'); // Show the submenu
             }
         });
-    });
-
-    // Close the submenu if clicking outside of it
-    window.addEventListener('click', function (event) {
-        if (!event.target.matches('.menu-item-dropdown > a') && !event.target.closest('.submenu')) {
-            document.querySelectorAll('.submenu').forEach(function (submenu) {
-                submenu.classList.remove('show');
-            });
-        }
-    });
-
-    // Close dropdown when resizing to wider screens
-    window.addEventListener('resize', function () {
-        if (window.innerWidth > 768) {
-            document.querySelectorAll('.submenu').forEach(function (submenu) {
-                submenu.classList.remove('show');
-            });
-        }
     });
 });
