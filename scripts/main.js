15034d629089a2bf29bc78d371987a7123239274
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-27 19:40:38 +0000

    Fix JS for header

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const dropdown = document.querySelector('.dropdown > a');
-    const dropdownContent = document.querySelector('.dropdown-content');
+    const menuToggle = document.querySelector('.menu-toggle');
+    const dropdown = document.querySelector('.menu-item-dropdown > a');
+    const dropdownContent = document.querySelector('.submenu');
 
-    // Toggle hamburger menu
-    hamburger.addEventListener('click', function () {
+    // Toggle main menu
+    menuToggle.addEventListener('click', function () {
         this.classList.toggle('active');
-        const navUl = document.querySelector('nav ul');
-        navUl.classList.toggle('show');
+        const mainMenu = document.querySelector('.main-menu');
+        mainMenu.classList.toggle('show');
     });
 
-    // Toggle dropdown on mobile
+    // Toggle submenu on mobile
     dropdown.addEventListener('click', function (event) {
         // Prevent default link behaviour only on mobile
         if (window.innerWidth <= 768) {
@@ -19,15 +19,15 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Close the dropdown if clicking outside of it
+    // Close the submenu if clicking outside of it
     window.addEventListener('click', function (event) {
-        if (!event.target.matches('.dropdown > a') && !event.target.closest('.dropdown-content')) {
+        if (!event.target.matches('.menu-item-dropdown > a') && !event.target.closest('.submenu')) {
             dropdownContent.classList.remove('show');
         }
     });
 
     // Close dropdown when resizing to wider screens
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         if (screenWidth > 768) {
             dropdownContent.classList.remove('show');
