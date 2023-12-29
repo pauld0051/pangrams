5b1c18f6860563c24a5d665d631119de14ef399e
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-29 11:17:07 +0100

    Added bubble, square, parenthesis, gothic, wide functionality

---
+++ scripts/wide.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const input = document.getElementById('textInput');
+  const outputElement = document.getElementById('alternatingTextOutput');
+  const copyButton = document.getElementById('copyButton');
+
+  document.getElementById('alternatingTextForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Use the Wide variant for transformation
+    outputElement.textContent = toUnicodeVariant(input.value, 'fullwidth');
+    outputElement.style.height = outputElement.scrollHeight + 'px'; // Adjust the height to fit content
+  });
+
+  copyButton.addEventListener('click', function () {
+    navigator.clipboard.writeText(outputElement.textContent).then(() => {
+      copyButton.textContent = 'Copied!';
+      setTimeout(() => {
+        copyButton.textContent = 'Copy Text';
+      }, 5000);
+    }).catch(err => {
+      console.error('Error copying text: ', err);
+    });
+  });
+
+  input.addEventListener('input', function () {
+    outputElement.textContent = ''; // Clear output when input changes
+    copyButton.textContent = 'Copy Text';
+  });
+});
