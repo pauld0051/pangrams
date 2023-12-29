0287ab8508d1abff38254701f45dc08c78a333da
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-29 13:39:04 +0100

    Add square negative functionality

--- scripts/square.js
+++ scripts/square.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const input = document.getElementById('textInput');
   const outputElement = document.getElementById('alternatingTextOutput');
+  const outputElement1 = document.getElementById('alternatingTextOutput1'); // New text area for squared negative
   const copyButton = document.getElementById('copyButton');
+  const copyButton1 = document.getElementById('copyButton1'); // New copy button for squared negative
 
   document.getElementById('alternatingTextForm').addEventListener('submit', function (e) {
     e.preventDefault();
     // Use the Square variant for transformation
     outputElement.textContent = toUnicodeVariant(input.value, 'squared');
-    outputElement.style.height = outputElement.scrollHeight + 'px'; // Adjust the height to fit content
+    // Use the Squared Negative variant for transformation
+    outputElement1.textContent = toUnicodeVariant(input.value, 'squared negative'); // Alias 'qn' if it's the correct one
+    // Adjust the heights to fit content
+    outputElement.style.height = outputElement.scrollHeight + 'px';
+    outputElement1.style.height = outputElement1.scrollHeight + 'px';
   });
 
   copyButton.addEventListener('click', function () {
@@ -21,8 +27,24 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  // New event listener for copying squared negative text
+  copyButton1.addEventListener('click', function () {
+    navigator.clipboard.writeText(outputElement1.textContent).then(() => {
+      copyButton1.textContent = 'Copied!';
+      setTimeout(() => {
+        copyButton1.textContent = 'Copy Squared Negative Text';
+      }, 5000);
+    }).catch(err => {
+      console.error('Error copying text: ', err);
+    });
+  });
+
   input.addEventListener('input', function () {
-    outputElement.textContent = ''; // Clear output when input changes
+    // Clear outputs when input changes
+    outputElement.textContent = '';
+    outputElement1.textContent = '';
+    // Reset button texts
     copyButton.textContent = 'Copy Text';
+    copyButton1.textContent = 'Copy Squared Negative Text';
   });
 });
