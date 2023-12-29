8a2724b0fca1809acc3708c5d86fea1d6d47a465
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-29 13:28:18 +0100

    Make bold gothic possible

--- scripts/gothic.js
+++ scripts/gothic.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
   const input = document.getElementById('textInput');
   const outputElement = document.getElementById('alternatingTextOutput');
+  const outputBoldElement = document.getElementById('alternatingTextOutput1');
   const copyButton = document.getElementById('copyButton');
+  const copyBoldButton = document.getElementById('copyBoldButton');
 
   document.getElementById('alternatingTextForm').addEventListener('submit', function (e) {
     e.preventDefault();
     // Use the Gothic variant for transformation
     outputElement.textContent = toUnicodeVariant(input.value, 'gothic');
+    // Use the Gothic Bold variant for transformation
+    outputBoldElement.textContent = toUnicodeVariant(input.value, 'gothic bold');
     outputElement.style.height = outputElement.scrollHeight + 'px'; // Adjust the height to fit content
+    outputBoldElement.style.height = outputBoldElement.scrollHeight + 'px'; // Adjust the height to fit content
   });
 
   copyButton.addEventListener('click', function () {
@@ -21,8 +26,21 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  copyBoldButton.addEventListener('click', function () {
+    navigator.clipboard.writeText(outputBoldElement.textContent).then(() => {
+      copyBoldButton.textContent = 'Copied!';
+      setTimeout(() => {
+        copyBoldButton.textContent = 'Copy Bold Text';
+      }, 5000);
+    }).catch(err => {
+      console.error('Error copying text: ', err);
+    });
+  });
+
   input.addEventListener('input', function () {
-    outputElement.textContent = ''; // Clear output when input changes
+    outputElement.textContent = ''; // Clear original output when input changes
+    outputBoldElement.textContent = ''; // Clear bold output when input changes
     copyButton.textContent = 'Copy Text';
+    copyBoldButton.textContent = 'Copy Bold Text';
   });
 });
