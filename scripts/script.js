a447abb4a3cd3718deac23d025edbc58719fd783
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-30 11:35:55 +0100

    Add script text functionality

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('alternatingTextForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let inputText = document.getElementById('textInput').value;
+
+    // Convert input text to its script equivalent using toUnicodeVariant
+    let scriptText = toUnicodeVariant(inputText, 'script');
+
+    // Set the script text as the value of the output element
+    const outputElement = document.getElementById('alternatingTextOutput');
+    outputElement.textContent = scriptText; // Use textContent to set text
+    outputElement.style.height = ""; // Reset the height
+    outputElement.style.height = outputElement.scrollHeight + "px"; // Adjust the height to fit content
+  });
+
+  // Copy to clipboard functionality
+  document.getElementById('copyButton').addEventListener('click', function () {
+    const textToCopy = document.getElementById('alternatingTextOutput').textContent;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      // Success feedback
+      this.textContent = 'Copied!';
+      setTimeout(() => {
+        this.textContent = 'Copy Text';
+      }, 5000);
+    }).catch(err => {
+      console.error('Failed to copy text: ', err);
+    });
+  });
+
+  // Event listener to revert the copy button text when the input is changed
+  document.getElementById('textInput').addEventListener('input', function () {
+    document.getElementById('copyButton').textContent = 'Copy Text';
+  });
+});
