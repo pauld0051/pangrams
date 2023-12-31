d2fa9751dde736c46caf3c22d3fc39b4c8413dd1
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2023-12-31 11:01:54 +0100

    Fix footer year output.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
-
 // Function to load and initialize header and footer
 function loadHeaderAndFooter() {
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('globalHeader').innerHTML = data;
-            initializeMenu(); // Initialize the menu after the header is loaded
+            // Any header-specific JavaScript can go here
         });
 
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('globalFooter').innerHTML = data;
-            // Any footer-specific JavaScript can go here
+            // Update the year in the footer
+            document.getElementById('currentYear').textContent = new Date().getFullYear();
         });
 }
 
